6ee6e0513953e45638770327a3d0b94a589e4da4
Allixdx <allison.sanroman@outlook.com>
2024-07-17 10:50:24 -0600

    se unen users y se muestran

--- src/app/components/unirse-sala/unirse-sala.component.ts
+++ src/app/components/unirse-sala/unirse-sala.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoteriaService } from 'src/app/service/loteria.service';
 import { Router } from '@angular/router';
 import { SocketService } from 'src/app/service/socket.service';
+import { Subscription } from 'rxjs';
+
+interface Player {
+  id: number;
+  name: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-unirse-sala',
   templateUrl: './unirse-sala.component.html',
   styleUrls: ['./unirse-sala.component.scss']
 })
-export class UnirseSalaComponent implements OnInit {
+export class UnirseSalaComponent implements OnInit, OnDestroy {
 
   joinRoomForm: FormGroup;
   errorMessage: string | null = null;
   userData: any | null = null;
+  unido: boolean = false;
+  jugadores: Player[] = [];
+  private jugadorUnidoSubscription: Subscription;
 
   constructor(
     private fb: FormBuilder, 
@@ -24,6 +34,7 @@ export class UnirseSalaComponent implements OnInit {
     this.joinRoomForm = this.fb.group({
       codigo: ['', Validators.required]
     });
+    this.jugadorUnidoSubscription = new Subscription();
   }
 
   ngOnInit(): void {}
@@ -40,9 +51,16 @@ export class UnirseSalaComponent implements OnInit {
             name: response.name,
             email: response.email,
           };
-          console.log(this.userData)
+          console.log(this.userData);
           this.socketService.connect();
           this.emitJugadorUnido();
+          this.unido = true;
+
+          // Suscribirse al evento de jugador unido
+          this.jugadorUnidoSubscription = this.socketService.onJugadorUnido().subscribe((data: Player) => {
+            this.jugadores.push(data);
+            console.log('Jugador unido:', data);
+          });
         },
         error: (err) => {
           console.error('Error:', err);
@@ -53,7 +71,11 @@ export class UnirseSalaComponent implements OnInit {
   }
 
   emitJugadorUnido(): void {
-    console.log("aaaa")
     this.socketService.emitJugadorUnido(this.userData);
+  }
+
+  ngOnDestroy(): void {
+    this.jugadorUnidoSubscription.unsubscribe();
+    this.socketService.disconnect();
   }
 }
