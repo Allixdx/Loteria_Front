fa01abb4a47a11a752ac9da62d5e3642e5426fda
Allixdx <allison.sanroman@outlook.com>
2024-07-28 20:13:05 -0600

    aparecen los frijoles jsjs

--- src/app/components/jugando/player/player.component.ts
+++ src/app/components/jugando/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LoteriaService } from 'src/app/service/loteria.service';
 
@@ -11,6 +11,11 @@ export class PlayerComponent implements OnInit {
   roomId: number | null = null;
   cartas: any[] = [];
   tablaCartas: any[][] = [];
+  frijolitos: { top: number, left: number }[] = [];
+  isDragging: boolean = false;
+  currentFrijolito: any | null = null;
+  offsetX: number = 0;
+  offsetY: number = 0;
 
   constructor(private route: ActivatedRoute, private loteriaService: LoteriaService) {}
 
@@ -24,9 +29,11 @@ export class PlayerComponent implements OnInit {
       this.cartas = cartas;
       this.shuffleAndCreateCardTable(); // Inicializa la tabla de cartas al cargar
     });
+
+    this.initializeFrijolitos();
   }
 
-  shuffleArray(array: any[]): any[] {
+  private shuffleArray(array: any[]): any[] {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
@@ -34,7 +41,7 @@ export class PlayerComponent implements OnInit {
     return array;
   }
 
-  shuffleAndCreateCardTable(): void {
+  private shuffleAndCreateCardTable(): void {
     this.cartas = this.shuffleArray(this.cartas).slice(0, 16);
     this.createCardTable();
   }
@@ -47,6 +54,35 @@ export class PlayerComponent implements OnInit {
         this.tablaCartas[i][j] = this.cartas[i * 4 + j];
       }
     }
+  }
+
+  private initializeFrijolitos(): void {
+    this.frijolitos = Array.from({ length: 16 }, () => ({
+      top: Math.random() * 100,
+      left: Math.random() * 100
+    }));
+  }
+
+  onMouseDown(event: MouseEvent, frijolito: any): void {
+    this.isDragging = true;
+    this.currentFrijolito = frijolito;
+    this.offsetX = event.clientX - frijolito.left;
+    this.offsetY = event.clientY - frijolito.top;
+    event.preventDefault();
+  }
+
+  @HostListener('window:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent): void {
+    if (this.isDragging && this.currentFrijolito) {
+      this.currentFrijolito.left = event.clientX - this.offsetX;
+      this.currentFrijolito.top = event.clientY - this.offsetY;
+    }
+  }
+
+  @HostListener('window:mouseup')
+  onMouseUp(): void {
+    this.isDragging = false;
+    this.currentFrijolito = null;
   }
 
   shuffleCards(): void {
